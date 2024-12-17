70d85a07dd8a69564b4def07d6912df04412c35f
Ranjan <khatiwadaranjan45@gmail.com>
2024-12-17 08:09:07 +0545

    remove

--- django-nginx-cash/demo/demoapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from .models import *
-
-def portfolio(request):
-    
-    return render(request, 'p1.html')
-
--- two_django_project_handle/demo1/demo1app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Demo1AppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "demo1app"
